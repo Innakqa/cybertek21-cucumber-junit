4766bd8d54747b351af7b0ce3225f91efd9afa8f
Innakqa <innakokoreva@gmail.com>
2021-02-25 00:12:11 -0500

    cucumber-junit update: driverPool

---
+++ src/test/java/com/cybertek/tests/ExcelWrites.java
@@ -0,0 +1,50 @@
+package com.cybertek.tests;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ExcelWrites {
+
+    XSSFWorkbook workbook;
+    XSSFSheet sheet;
+    XSSFRow row;
+    XSSFCell cell;
+
+    @Test
+    public void excel_write() throws IOException {
+
+        String path = "SampleData.xlsx";
+
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+        //workbook > sheet > row > cell
+
+        //Loading excel workbook into class
+        workbook = new XSSFWorkbook(fileInputStream);
+
+        //open the sheet using workbook object
+        sheet = workbook.getSheet("Employees");
+
+        //kings row
+        row = sheet.getRow(1);
+
+        //kings cell
+        cell = row.getCell(1);
+
+        //ex: Storing Adams cell if we were to be reusing it
+        XSSFCell adamsCell = sheet.getRow(2).getCell(0);
+        System.out.println("Before = " + adamsCell);
+
+
+
+
+    }
+
+
+}
